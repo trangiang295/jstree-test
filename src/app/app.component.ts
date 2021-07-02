89201c953fac41ad34cfb02306eefb81693a6cdd
Tran Giang <trangiang295@gmail.com>
2021-07-02 23:43:30 +0700

    add tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jstree-test';
+
+  ngOnInit() {
+    $('#mytree').jstree({core: {
+      data: (node: any, cb: any) => {
+        cb.call(this, this.getChild());
+      }
+    }});
+  }
+
+  getChild() {
+      return [
+        {text: 'root1', children: true},
+        {text: 'root2'},
+      ]
+  }
+
+  // getChild() {
+  //   setTimeout(() => {
+  //     return [
+  //       {text: 'root1', children: true},
+  //       {text: 'root2'},
+  //     ]
+  //   }, 3000);
+  // }
 }
+
